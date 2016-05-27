002b4dd84f878f739f631797dd5b8dee6f9744a8
codymalick <cody.malick@gmail.com>
2016-05-27 15:09:01 -0700

    Preprocessing code complete

--- project3/main.py
+++ project3/main.py
@@ -1,9 +1,9 @@
-print("test")
+
 
 def parse_data(data):
 	parsed_data = []
 	for line in data:
-		parsed = line.translate(None, '()".\'!?+-=\\:/_;')
+		parsed = line.translate(None, '()".\'!?+-=\\:/_;%&$')
 		#parsed = parsed.translate(None, ')')
 		parsed = parsed.split(',')
 		parsed_data.append(parsed)
@@ -26,14 +26,49 @@ def create_vocab(data):
 
 			if added == False:
 				vocab.append([word,1])
-	print vocab
-	#print(words)
+
+	vocab.sort()
+	return vocab
+
+def vector_create(vocab):
+	feature_vector = []
+	#Feature vector is the length of vocab+1, where length + 1 is the classification
+	for i in range(0, len(vocab)+1):
+		feature_vector.append(0)
+	return feature_vector
+
+def output_preprocessed_set(vocab, training, file_name):
+	f = open(file_name, 'w')
+	for value in vocab:
+		f.write(value[0] + ',')
+
+	f.write('\n')
+
+	for sentence in training:
+		words = sentence[0].split(' ')
+		#create the training set output
+		output = vector_create(vocab)
+		for word in words:
+			for i in range(0, len(output)-1):
+				#print(word, vocab[i][0])
+				if vocab[i][0] == word:
+					output[i] = 1
+					#print(word)
+		f.write("%s\n" % output)
+	f.close()
 
 def main():
-	#Read in the test data
-	test = open("training_text.txt").read().splitlines()
-	data = parse_data(test)
+	#Read in the training data
+	train = open("training_text.txt").read().splitlines()
+	data = parse_data(train)
+	#Create the vocabulary from the training set
 	vocab = create_vocab(data)
-	#print(data)
+	output_preprocessed_set(vocab, data, "preprocessed_train.txt")
+
+	#Read in the test data
+	test = open("test_text.txt").read().splitlines()
+	t_data = parse_data(test)
+
+	output_preprocessed_set(vocab, t_data, "preprocessed_test.txt")
 
 main()
